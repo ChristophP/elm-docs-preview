137c0a0d23c8c22679c4fd42621f40b89509e901
ChristophP <deedop@hotmail.de>
2017-12-23 14:47:36 +0100

    build rough pipeline using webpack

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 const { promisify } = require('util');
+const childProcess = require('child_process');
+const webpack = promisify(require('webpack'));
+const createConfig = require('./webpack.config.js');
 
 const elmStaticHtml = require('elm-static-html-lib').default;
 
 const readFileAsync = promisify(fs.readFile);
+const exec = promisify(childProcess.exec);
 
-readFileAsync('./docs.json')
-  .then(json => JSON.parse(json))
-  .then(model => {
-    const options = { model, decoder: 'Decoder.decodeModel' };
+const dir = process.argv[2];
+if (!dir) {
+  console.log("Please pass a directory name where you're elm-package.json is.");
+}
 
-    return elmStaticHtml(
-      process.cwd(),
-      'View.view',
-      options
-    ).then(generatedHtml => {
-      fs.writeFileSync('docs.html', generatedHtml);
-    });
-  });
+const docsFile = 'docs_tmp.json';
+
+exec(`elm make --docs=${docsFile}`, { cwd: dir })
+  .then(console.log)
+  .then(() => fs.readFileSync(docsFile, 'utf8'))
+  .then(data => webpack(createConfig(JSON.parse(data))))
+  .then(() => console.log('webpack done'))
+  .catch(console.log)
+  .then(() => process.exit(0));
+
+//readFileAsync('./docs.json')
+//.then(json => JSON.parse(json))
+//.then(model => {
+//const options = { model, decoder: 'Decoder.decodeModel' };
+
+//return elmStaticHtml(
+//process.cwd(),
+//'View.view',
+//options
+//).then(generatedHtml => {
+//fs.writeFileSync('docs.html', generatedHtml);
+//});
+//});
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
+// see: https://github.com/webpack/webpack/issues/2537
+const isProd = process.argv.indexOf('-p') !== -1;
+
+module.exports = data => ({
+  entry: {
+    bundle: './src/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'Advent',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: {
+          loader: 'elm-webpack-loader',
+          options: isProd
+            ? {}
+            : {
+                debug: true,
+                warn: true,
+              },
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+            plugins: ['transform-runtime'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: ['css-loader'],
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Advent',
+      template: 'src/index.html.ejs',
+      filename: 'index.html',
+      data,
+      inject: 'head',
+      minify: {
+        html5: true,
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyJS: true,
+        minifyCSS: true,
+      },
+      inlineSource: '.(js|css)$', // embed all javascript and css inline
+    }),
+    new HtmlWebpackInlineSourcePlugin(),
+    new ExtractTextPlugin('[name].css'),
+  ],
+});
